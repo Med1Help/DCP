8d4b679a55b9499ed21846ea5df72dc43aa01b75
mohammed <mohammed@aza.ma>
2024-09-26 11:04:56 +0100

    init

---
+++ src/main/java/mohammed/ma/DCP/DcpApplication.java
@@ -0,0 +1,13 @@
+package mohammed.ma.DCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mohammed/ma/DCP/DcpApplicationTests.java
@@ -0,0 +1,13 @@
+package mohammed.ma.DCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
